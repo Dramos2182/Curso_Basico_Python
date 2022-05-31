c62466de905f096028ecd59aaeece1603870e3a0
Dramos2182 <eduardo.ramosm@uanl.edu.mx>
2022-05-30 22:24:58 -0500

    initial commit

---
+++ holamundo.py
@@ -0,0 +1,3 @@
+#Comentarios con hashtag
+nombre = str(input("Ingresa un nombre: "))
+print(f"Bienvenido {nombre}")
---
+++ tipos DeVariables.py
@@ -0,0 +1,17 @@
+# String - Cadena de caracteres, palabras, etc
+nombre="David 👌"
+
+#Enteros - Numeros sin punto decimal
+edad = 19
+
+#Flotante - Numeros con punto decimal
+pi = 3.1416
+
+edad= float(edad) #Casteo, transformar un tipo de variable a otro
+
+print(edad)
+print(type(nombre))
+
+#Bool - verdadero o falso
+Legustas= False
+print(Legustase)
